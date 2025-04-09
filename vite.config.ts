c75b8f15ee116121ddbb40bfa0dd855bdd058c35
Andrea Laretto <iwilare@gmail.com>
2025-04-09 12:32:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/launchpad-midi/',
+  base: '/launchpad/',
   server: {
     port: 3000,
     open: true,
